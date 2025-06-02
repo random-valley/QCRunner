7fe036ed5578f52838e7d86d972d513320b2f41b
BlakeAHalliday <halliday@quantumbase.co.uk>
2025-06-02 11:45:44 +0100

    Initial Commit

---
+++ QCRunner/QCRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QCRunnerApp.swift
+//  QCRunner
+//
+//  Created by Blake on 02/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct QCRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
